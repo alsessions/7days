55fe53a5d32060ac66c300aa4246d1858a53b3b9
Al Sessions <fultonchain@gmail.com>
2024-11-04 18:26:17 -0500

    Font, header and footer, CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   content: [],
   theme: {
+    fontFamily: {
+      sans: [ 'sans-serif'],
+      display: ['Averia Libre', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [
